97d9276635470ec5171badbfbbcbd204b307ecf9
benoit layer <benoit.layer@gmail.com>
2015-01-23 20:36:02 +0100

    Initial Commit

---
+++ StepColor/StepColor.h
@@ -0,0 +1,19 @@
+//
+//  StepColor.h
+//  StepColor
+//
+//  Created by Benoit Layer on 23/01/2015.
+//  Copyright (c) 2015 Benoit Layer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StepColor.
+FOUNDATION_EXPORT double StepColorVersionNumber;
+
+//! Project version string for StepColor.
+FOUNDATION_EXPORT const unsigned char StepColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StepColor/PublicHeader.h>
+
+
